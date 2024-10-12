d88d322ac35f74ab8075c93bc945197be913f2aa
marilyn.musyimi <marilyn.musyimi@student.moringaschool.com>
2024-10-12 09:47:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Use reduce to calculate the total number of batteries
+const totalBatteries = batteryBatches.reduce((total, batch) => {
+    return total + batch;
+}, 0); // Starting with an initial value of 0
+
+// Output the total number of batteries
+console.log(totalBatteries); // This will log the total count
